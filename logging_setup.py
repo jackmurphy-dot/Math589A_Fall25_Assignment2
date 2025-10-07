6737fe77d88b7c997df62b4bcd0bd43c5034e22e
jackmurphy-dot <jackmurphy669@gmail.com>
2025-10-07 12:14:18 -0700

    Update logging_setup.py

--- logging_setup.py
+++ logging_setup.py
@@ -1,5 +1,5 @@
-# logging_setup.py
 import logging
+
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)

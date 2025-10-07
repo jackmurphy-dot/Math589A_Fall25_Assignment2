a96ed65f1a286c502607822c58aa5e47405c3293
jackmurphy-dot <jackmurphy669@gmail.com>
2025-10-07 12:12:52 -0700

    Update logging_setup.py

--- logging_setup.py
+++ logging_setup.py
@@ -1,22 +1,6 @@
-# File: logging_setup.py
-# Insert this code at the top of your implementation file.
-# Alternatively, import this file as a module (even better).
-# (most likely, general_linear_solver.py) to enable logging.
-# REMEMBER: Don't print, log.
-
-import numpy as np
-
-# level: Sets the minimum severity level for messages to be processed
-# (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL). Messages below this
-# level will be ignored.
+# logging_setup.py
 import logging
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-# logging.basicConfig(level=logging.INFO,
-#                     format='%(asctime)s - %(levelname)s - %(message)s',
-#                     handlers=[logging.FileHandler("app.log"),
-#                               logging.StreamHandler()])
-
+                    format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)  # Set the logger's level
+logger.setLevel(logging.INFO)

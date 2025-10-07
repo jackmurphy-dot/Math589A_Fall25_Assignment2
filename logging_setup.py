f3aebcb68383f50cd4f2a388c05ff9dd8c0df1f9
jackmurphy-dot <jackmurphy669@gmail.com>
2025-10-07 16:32:31 -0700

    Update logging_setup.py

--- logging_setup.py
+++ logging_setup.py
@@ -1,6 +1,16 @@
 import logging
+import numpy as np
 
+# level: Sets the minimum severity level for messages to be processed
+# (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL). Messages below this
+# level will be ignored.
 logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s - %(levelname)s - %(message)s")
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+# logging.basicConfig(level=logging.INFO,
+#                     format='%(asctime)s - %(levelname)s - %(message)s',
+#                     handlers=[logging.FileHandler("app.log"),
+#                               logging.StreamHandler()])
+
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO) # Set the logger's level
